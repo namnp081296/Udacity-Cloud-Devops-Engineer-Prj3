934dcedf364c57bd58dad9443a3cd796148af8fe
Nam Nguyen Phuong <namnp081296@gmail.com>
2024-03-07 10:41:36 +0700

    Build Project 3

--- analytics/config.py
+++ analytics/config.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 #db_username = os.environ.get["DB_USERNAME", "postgres"]
 #db_password = os.environ.get["DB_PASSWORD", "zRodlTHX0d"]
 db_username = os.environ.get("DB_USERNAME", "postgres")
-db_password = os.environ.get("DB_PASSWORD", "SStTJaCN9N")
+db_password = os.environ.get("DB_PASSWORD", "Bui0Jf6iwE")
 # db_host = os.environ.get("DB_HOST", "127.0.0.1")
 db_host = os.environ.get("DB_HOST", "project3-postgres-postgresql")
 db_port = os.environ.get("DB_PORT", "5432")
