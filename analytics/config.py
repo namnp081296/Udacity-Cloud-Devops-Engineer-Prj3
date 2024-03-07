254b7b247968ee5f0a67450e360aba919cfc317b
Nam Nguyen Phuong <namnp081296@gmail.com>
2024-03-07 08:44:17 +0700

    Change host name value

--- analytics/config.py
+++ analytics/config.py
@@ -8,7 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 #db_password = os.environ.get["DB_PASSWORD", "zRodlTHX0d"]
 db_username = os.environ.get("DB_USERNAME", "postgres")
 db_password = os.environ.get("DB_PASSWORD", "SStTJaCN9N")
-db_host = os.environ.get("DB_HOST", "127.0.0.1")
+# db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_host = os.environ.get("DB_HOST", "project3-postgres-postgresql")
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
